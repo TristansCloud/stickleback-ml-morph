19702288c8fb2a040bc0ec1ab6bcf2df26947393
Tristan Kosciuch <tkosciuch@luc.edu>
2022-11-28 06:18:28 +0000

    default hyperparameters reccomended by A. Porto

--- n_fold_cv.py
+++ n_fold_cv.py
@@ -10,12 +10,15 @@
 #   - parameters used for shape_trainer.py
 
 # Steps:
-#   1. copy all photos to a new directory called train in the output directory
-#   2. move 1/n of photos to a test directory
-#   3. generate train and test.xml
-#   4. train model
-#   5. move test photos back to train directory
-#   6. repeat from step 2
+#   1. Copy all photos to a new directory called train in the output directory
+#   2. Move 1/n of photos to a test subdirectory of the output directory
+#   3. Generate train.xml and test.xml
+#   4. Train model
+#   5. Move test photos back to train directory
+#   6. Repeat from step 2 for all n folds. Each fold is unique,
+#      the images are lexicographically sorted and every nth file
+#      is moved for a fold. The starting file shifts by one after
+#      each fold.
 
 import utils
 import argparse
@@ -34,20 +37,20 @@ ap.add_argument("-n", "--nfold", type=int, default=10,
 ap.add_argument("-o", "--out", type=str,
     help="output directory, defaults to 'output${n-folds}", metavar='')
 # flags for shape_trainer.py
-ap.add_argument("-th", "--threads", type=int, default=1,
-    help="number of threads to be used (default = 1)", metavar='')
+ap.add_argument("-th", "--threads", type=int, default=2,
+    help="number of threads to be used (default = 2)", metavar='')
 ap.add_argument("-dp", "--tree-depth", type=int, default=4,
     help="choice of tree depth (default = 4)", metavar='')
-ap.add_argument("-c", "--cascade-depth", type=int, default=15,
-    help="choice of cascade depth (default = 15)", metavar='')
+ap.add_argument("-c", "--cascade-depth", type=int, default=30,
+    help="choice of cascade depth (default = 30)", metavar='')
 ap.add_argument("-nu", "--nu", type=float, default=0.1,
     help="regularization parameter (default = 0.1)", metavar='')
-ap.add_argument("-os", "--oversampling", type=int, default=10,
-    help="oversampling amount (default = 10)", metavar='')
-ap.add_argument("-s", "--test-splits", type=int, default=20,
-    help="number of test splits (default = 20)", metavar='')
-ap.add_argument("-f", "--feature-pool-size", type=int, default=500,
-    help="choice of feature pool size (default = 500)", metavar='')
+ap.add_argument("-os", "--oversampling", type=int, default=200,
+    help="oversampling amount (default = 200)", metavar='')
+ap.add_argument("-s", "--test-splits", type=int, default=30,
+    help="number of test splits (default = 30)", metavar='')
+ap.add_argument("-f", "--feature-pool-size", type=int, default=1000,
+    help="choice of feature pool size (default = 1000)", metavar='')
 ap.add_argument("-nt", "--num-trees", type=int, default=500,
     help="number of regression trees (default = 500)", metavar='')
 args = vars(ap.parse_args())
@@ -96,10 +99,12 @@ for n in range(args["nfold"]):
     #   4. train model
     cmd  = "python3 shape_trainer.py -d " + "train.xml -t " + "test" + str(n) + ".xml -o " + "predictor" + str(n) + " -th " + str(args["threads"]) + " -dp " + str(args["tree_depth"]) + " -c " + str(args["cascade_depth"]) + " -nu " + str(args["nu"]) + " -os " + str(args["oversampling"]) + " -s " + str(args["test_splits"]) + " -f " + str(args["feature_pool_size"]) + " -n " + str(args["num_trees"])
     print(cmd)
-    os.system(cmd)    
+    os.system(cmd)
+       
 
     #   5. move test photos back to train directory
     bck = os.listdir(out + "/test" + str(n))
     for x in bck:
         shutil.move(os.path.join(out + "/test" + str(n), x), out + "/train")
-
+#     shutil.move(test) 
+# os.remove("train.xml")
